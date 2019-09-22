bb345cf2478fe7f4fa54069534237365fa3e4c70
Park YoungIn <yountpark@gmail.com>
2019-09-22 22:14:21 +0900

    기존 레이아웃에 QR코드 버튼에 QR코드 생성 레이아웃 만들기 및 연결

--- HealDoc_mobile_5/app/src/main/java/com/example/healdoc_mobile_5/MainActivity.kt
+++ HealDoc_mobile_5/app/src/main/java/com/example/healdoc_mobile_5/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.healdoc_mobile_5
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_QR.setOnClickListener(this)
     }
+
+    override fun onClick(view: View){
+        when (view.id){
+            R.id.btn_QR -> {
+                val intent = Intent(this, QrReaderActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
 }
