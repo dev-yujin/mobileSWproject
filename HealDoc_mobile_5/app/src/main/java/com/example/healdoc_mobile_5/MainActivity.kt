5c0d4d0da9deac88d745bbcde5c4131639b537e4
Park YoungIn <yountpark@gmail.com>
2019-10-04 22:39:49 +0900

    firebase와 연결

--- HealDoc_mobile_5/app/src/main/java/com/example/healdoc_mobile_5/MainActivity.kt
+++ HealDoc_mobile_5/app/src/main/java/com/example/healdoc_mobile_5/MainActivity.kt
@@ -9,6 +9,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
