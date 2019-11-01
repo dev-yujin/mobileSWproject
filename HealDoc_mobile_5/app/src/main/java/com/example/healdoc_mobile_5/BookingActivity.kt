0e869226ebfc9c7e853af57d02e5dab66e3a441b
likey0u96 <kyouj96@gmail.com>
2019-11-01 18:44:36 +0900

    예약하기 화면 스피너 레이아웃 수정

--- HealDoc_mobile_5/app/src/main/java/com/example/healdoc_mobile_5/BookingActivity.kt
+++ HealDoc_mobile_5/app/src/main/java/com/example/healdoc_mobile_5/BookingActivity.kt
@@ -1,13 +1,79 @@
+//예약하기 첫 화면 (진료과 선택, 선생님 선택)
 package com.example.healdoc_mobile_5
 
 //import android.support.v7.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_booking.*
 
 class BookingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking)
+
+        //날짜선택 버튼 클릭 이벤트
+        btn_sel_date.setOnClickListener {  }
+
+
+        var sel_sub : String? = ""
+        var sel_tea : String? = ""
+
+        //스피너 셋팅
+        //val spinSubject = resources.getStringArray(R.array.subjectList)
+        //val spinTeacher = resources.getStringArray(R.array.teacherList)
+        val spinSubject = findViewById(R.id.spin_subject) as Spinner
+        val spinTeacher = findViewById(R.id.spin_teacher) as Spinner
+        val adapterSubject = ArrayAdapter.createFromResource(this, R.array.subjectList, R.layout.item_spinner)
+        val adapterTeacher = ArrayAdapter.createFromResource(this, R.array.teacherList, R.layout.item_spinner)
+
+        spinSubject.setSelection(0) //초기값
+        spinTeacher.setSelection(0)
+
+        //진료과목 선택 스피너
+        spinSubject.adapter = adapterSubject
+        spinSubject.prompt = "진료과목을 선택해주세요"
+        spinSubject.onItemSelectedListener = object :AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //string을 저장
+                //sel_sub = view[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+        }
+
+        //선생님 선택
+        spinTeacher.adapter = adapterTeacher
+        spinTeacher.prompt = "선생님을 선택해주세요"
+        spinTeacher.onItemSelectedListener = object :AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //string을 저장
+                //sel_sub = view[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
     }
+
+
+
+
 }
