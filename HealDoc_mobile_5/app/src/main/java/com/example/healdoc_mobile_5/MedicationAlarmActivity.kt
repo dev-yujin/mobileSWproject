9ac07a237384837e5a406f41faaeb46adcced781
Park YoungIn <yountpark@gmail.com>
2019-11-25 04:17:59 +0900

    부작용 알리미 - 레이아웃 구성

---
+++ HealDoc_mobile_5/app/src/main/java/com/example/healdoc_mobile_5/MedicationAlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.example.healdoc_mobile_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedicationAlarmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medication_alarm)
+    }
+}
